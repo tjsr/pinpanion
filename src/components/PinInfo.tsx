7dadac1f4e4fcf8ac68a0f7704ef50f8584a34f9
Tim Rowe <tim@tjsr.id.au>
2022-10-04 22:43:23 +1100

    Give pins a bit of colour.

--- src/components/PinInfo.tsx
+++ src/components/PinInfo.tsx
@@ -1,3 +1,5 @@
+import '../css/pincolours.css';
+
 import { PAX, Pin, PinSet, SizesType } from '../types';
 
 import React from 'react';
@@ -13,34 +15,39 @@ type PinInfoPropTypes = {
   children?: React.ReactNode;
 };
 
-export const PinInfo = ({
-  displaySize = 'normal',
-  pin,
-  paxs,
-  pinSets,
-  children,
-}: PinInfoPropTypes): JSX.Element => {
+const getPaxCssClass = (prefix: string, paxId: number): string => {
+  return prefix + eventnames[paxId].cssClass;
+};
+
+export const PinInfo = ({ displaySize = 'normal', pin, paxs, pinSets, children }: PinInfoPropTypes): JSX.Element => {
   const url = `${config.imagePrefix}/${pin.image_name.split('?')[0]}`;
-  const pinPax: PAX | undefined = paxs?.find(
-    (pax: PAX) => pax.id == pin.pax_id
-  );
+  const pinPax: PAX | undefined = paxs?.find((pax: PAX) => pax.id == pin.pax_id);
   const pinSet: PinSet | undefined = pinSets?.find((set: PinSet) => {
     return set?.id == pin.set_id || set?.id == pin.sub_set_id;
   });
 
-  const pinClasses = getPinClassForSize(displaySize);
+  let pinClasses = getPinClassForSize(displaySize);
+  let paxCssClass = 'pax';
+  let setCssClass = 'set';
+
+  if (pinPax) {
+    pinClasses = pinClasses + ' ' + getPaxCssClass('pin', pinPax.id!);
+    paxCssClass = paxCssClass + ' ' + getPaxCssClass('pax', pinPax.id!);
+    setCssClass = setCssClass + ' ' + getPaxCssClass('pax', pinPax.id!);
+  }
 
   return (
     <>
       <div className={pinClasses} id={`pin_${pin.id}`}>
         <div className="pinInfo">
           <h3>{pin.name}</h3>
-          {pinPax?.name ? (
-            <div className="pax">{eventnames[pinPax.id!].description}</div>
+          {pinSet?.name ? (
+            <div className={setCssClass}>{pinSet?.name}</div>
+          ) : pinPax ? (
+            <div className={paxCssClass}>{eventnames[pinPax.id!].description}</div>
           ) : (
             <></>
           )}
-          {pinSet?.name ? <div className="set">{pinSet?.name}</div> : <></>}
           <img className="pinImage" alt={pin.name} src={url} />
         </div>
         {children}
