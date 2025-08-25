f109b0b83874370731e6676c44844d35efe7746d
Tim Rowe <tim@tjsr.id.au>
2025-08-25 18:14:08 +1000

    Download latest version unless provided.

--- src/utils/dlschema.ts
+++ src/utils/dlschema.ts
@@ -20,15 +20,22 @@ async function downloadFile(url: string, outputPath: string) {
 }
 
 program
-  .description("Pinnypals schema downloader")
+  .description('Pinnypals schema downloader')
   .option('-l, --latest', 'Get the latest version from the server')
-  .option('-v, --version <version>', 'Get API of version', '1.1.3')
+  .option('-v, --version <version>', 'Get API of version')
   .option('-h, --host <host>', 'Host URL', 'https://api.pinnypals.com/')
   .option('-p, --path <path>', 'Schema file output path', './src/pinnypals')
   .action(async (options: SchemaToolCommandOptions, _command) => {
-    let version = options.version;
-    if (options.latest) {
-      const versionUrl = options.host + "/api/version";
+    let version: string | undefined;
+    if (options.version) {
+      if (options.latest) {
+        console.error('Cannot specify both --latest and --version');
+        process.exit(1);
+      }
+      version = options.version;
+      console.log(`Using specified version ${version}`);
+    } else {
+      const versionUrl = options.host + '/api/version';
       const response = await fetch(versionUrl);
       if (!response.ok) {
         throw new Error(`Failed to fetch ${versionUrl}: ${response.status} ${response.statusText}`);
@@ -38,17 +45,14 @@ program
       version = json.version;
       // Get the latest version from the server
       console.log(`Latest version returned from /api/version is ${version}`);
-    } else {
-      console.log(`Using specified version ${version}`);
     }
-    // Else use the provided version
 
     const schemaFileName = `pinnypals-openapi-${version}-public.json`;
     const url = `${options.host}/${schemaFileName}`;
     const outputPath = `${options.path}/${schemaFileName}`;
     downloadFile(url, outputPath)
       .then(() => console.log(`Downloaded ${url} to ${outputPath}`))
-      .catch(err => {
+      .catch((err) => {
         console.error('Error:', err.message);
         process.exit(1);
       });
